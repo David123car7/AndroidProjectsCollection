06cdb3ed53ea67b3f9db5e112100b12d7d2dfce8
CrZ <david123car7@gmail.com>
2025-11-07 12:35:13 +0000

    Add second button and toggle logic to AppView
    Introduces a second button to AppView that increments a counter. Refactors button text logic to toggle between two states and updates layout to use Box and Row with padding.

--- LearningProject/app/src/main/java/com/david/learningproject/AppView.kt
+++ LearningProject/app/src/main/java/com/david/learningproject/AppView.kt
@@ -1,5 +1,8 @@
 package com.david.learningproject
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,16 +12,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppView(name: String, modifier: Modifier = Modifier){
 
     var buttonText by remember { mutableStateOf("Kazzio") }
+    var stateText by remember { mutableStateOf(false) }
+    var buttonValueText by remember { mutableStateOf(0) }
 
-    var state = false
-
-    //Greeting(name, modifier)
-    AppButton(onClick = {buttonText = "Trolado"}, buttonText, modifier)
+    fun changeState(){
+        if(stateText){
+            buttonText = "Trolado"
+            stateText = false;
+        }
+        else{
+            buttonText = "Kazzio"
+            stateText = true;
+        }
+    }
+    Box(modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp)){
+        Row {
+            AppButton(onClick = { changeState() }, buttonText, modifier)
+            AppButton(onClick = { buttonValueText += 1 }, buttonValueText.toString(), modifier)
+        }
+    }
+    //AppButton(onClick = { buttonValueText += 1 }, buttonText, modifier)
 }
 
 @Composable
