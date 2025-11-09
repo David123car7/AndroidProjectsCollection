1af26a8341782d58acb64bb46137a9613fdb2834
CrZ <david123car7@gmail.com>
2025-11-09 11:59:14 +0000

    Initial commit for ProductsList-API Android app
    Set up a new Android project with Jetpack Compose, including Gradle configuration, theme files, launcher icons, basic MainActivity, and sample unit/instrumented tests. Project structure and dependencies are defined for future development.

---
+++ ProductsListAPI/app/src/main/java/com/david/productslist_api/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.david.productslist_api
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.david.productslist_api.ui.theme.ProductsListAPITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProductsListAPITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProductsListAPITheme {
+        Greeting("Android")
+    }
+}
